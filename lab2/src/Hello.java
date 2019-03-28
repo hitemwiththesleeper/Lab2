13b2c67cd7472aeb9adfe0a12e091a03e61f56f8
Gamma <Gamma@10.117.20.61>
2019-03-27 18:33:23 -0700

    sup1

--- lab2/src/Hello.java
+++ lab2/src/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("217495513 - This line was added by Ethan Luong.");
 		System.out.println("218694789 - This line was added by Cameron Bass.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("219571119 - This line was added by Victoria Margaryan.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
