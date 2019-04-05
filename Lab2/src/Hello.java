9b15b058f2bdb157d1e1d4ab90b183eb08972a0e
Cameron Bass <cameronlbass@csus.edu>
2019-04-05 14:47:24 -0700

    In Eclipse, moved a line, then pushed the change.

--- Lab2/src/Hello.java
+++ Lab2/src/Hello.java
@@ -4,9 +4,9 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("217495513 - This line was added by Ethan Luong.");
-		System.out.println("218694789 - This line was added by Cameron Bass.");
 		System.out.println("219571119 - This line was added by Victoria Margaryan.");
 		System.out.println("219734594 - This line was added by Sean Foley.");
+		System.out.println("218694789 - This line was added by Cameron Bass.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
